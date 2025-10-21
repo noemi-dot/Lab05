79cf09e11e9cb6d3071b5d0a97579da65022bf45
noemi-dot <noemirinzo29@gmail.com>
2025-10-21 14:12:44 +0200

    prime modifiche

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import flet as ft
 from alert import AlertManager
 from autonoleggio import Autonoleggio
+import flet as ft
 
 FILE_AUTO = "automobili.csv"
 
@@ -36,7 +36,11 @@ def main(page: ft.Page):
     lista_auto = ft.ListView(expand=True, spacing=5, padding=10, auto_scroll=True)
 
     # Tutti i TextField per le info necessarie per aggiungere una nuova automobile (marca, modello, anno, contatore posti)
-    # TODO
+    input_marca = ft.TextField(label="Marca")
+    input_modello = ft.TextField(label="Modello")
+    input_anno = ft.TextField(label="Anno")
+
+    #non si usa value, perchè non stiamo modificando dati già esistenti.
 
     # --- FUNZIONI APP ---
     def aggiorna_lista_auto():
@@ -58,14 +62,38 @@ def main(page: ft.Page):
         page.update()
 
     # Handlers per la gestione dei bottoni utili all'inserimento di una nuova auto
-    # TODO
+    #esattamente quello che c'è nelle slide. ppt slide 18
+    def handleAdd(e):
+        currentVal = txtOut.value
+
+        txtOut.value = currentVal + 1
+        txtOut.update()
+
+    def handleRemove(e):
+        currentVal = txtOut.value
+
+        txtOut.value = currentVal - 1
+        txtOut.update()
 
     # --- EVENTI ---
     toggle_cambia_tema = ft.Switch(label="Tema scuro", value=True, on_change=cambia_tema)
     pulsante_conferma_responsabile = ft.ElevatedButton("Conferma", on_click=conferma_responsabile)
 
     # Bottoni per la gestione dell'inserimento di una nuova auto
-    # TODO
+    #creo i bottoni + e -
+    #ppt 7 slide 19
+    btnMinus = ft.IconButton(icon=ft.Icons.REMOVE,
+                             icon_color="green",
+                             icon_size=24, on_click=handleRemove)
+    btnAdd = ft.IconButton(icon=ft.Icons.ADD,
+                           icon_color="green",
+                           icon_size=24, on_click=handleAdd)
+    txtOut = ft.TextField(width=100, disabled=True,
+                          value=0, border_color="green",
+                          text_align=ft.TextAlign.CENTER)
+    row1 = ft.Row([btnMinus, txtOut, btnAdd],
+                  alignment=ft.MainAxisAlignment.CENTER)
+    page.add(row1)
 
     # --- LAYOUT ---
     page.add(
@@ -83,7 +111,7 @@ def main(page: ft.Page):
                alignment=ft.MainAxisAlignment.CENTER),
 
         # Sezione 3
-        # TODO
+        ft.Text("Aggiungi nuova automobile", size=20),
 
         # Sezione 4
         ft.Divider(),
